1e0d6f1f3bc4b83a7bd1f1ede8e810327d813b7a
karllucas <karlkamakia@gmail.com>
2022-12-20 12:58:44 +0000

    submit 0-constants.js

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,22 @@
+# Consists of two functions: taskFirst and taskNext
+# The first holds const variable and second has let a let variable
+
+export function taskOne() {
+  const taskOne = 'I prefer const';
+  return taskOne;
+}
+
+export function taskTwo() {
+  const taskTwo = 'when I can.';
+  return taskTwo;
+}
+
+export function taskThree() {
+  let taskThree = 'But sometimes';
+  return taskThree;
+}
+
+export function taskThree() {
+  let taskThree = 'let is okay';
+  reurn taskThree;
+}
